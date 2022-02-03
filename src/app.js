395b1a55b2fe14d951644e0a4e18b8670ee8f5bc
atudor913 <atudor913@gmail.com>
2022-02-03 08:56:22 -0500

    Data integration
    I created a new function that pulls data from the api URL so that I can display the predicted forecast. First I created a call function getForecast( ), I then defined it by creating the function getForecast( ) and injected the api's data inside of it. Finally I facilitated the transfer of behavior to the previous function I created called showForecast( )...so that I can see the data being projected onto my super cute weather app.

--- src/app.js
+++ src/app.js
@@ -1,3 +1,96 @@
+/*DATE INTERGRATION*/
+let setting = new Date();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+let currentDay = days[setting.getDay()];
+let currentMin = setting.getMinutes();
+let currentHour = setting.getHours();
+let currentMonth = months[setting.getMonth()];
+
+let currentSettings = `${currentDay} ${currentHour}:${currentMin}`;
+console.log(currentSettings);
+
+let inputTime = document.querySelector("#time-date");
+inputTime.innerHTML = currentSettings;
+
+/*FORECAST*/
+
+function showForecast(response) {
+  console.log(response.data.daily);
+  let forecastElement = document.querySelector("#forecast-temps");
+  let forecastHTML = `<div class="row">`;
+  let days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  days.forEach(function (day) {
+    forecastHTML =
+      forecastHTML +
+      `  
+      <div class="col-2">
+            <div class="forecast_one" id="forecast-one">
+              <div class="day_one" id="day-one">${day}</div>
+              
+               <img src="http://openweathermap.org/img/wn/10d@2x.png"
+            alt=""
+            width="65"/>
+            </div>
+              <div class="forcast_temp">
+              <span id="max-temp">1° </span>
+              <span id="min-temp">8°</span>
+            </div>
+          </div>`;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+  forecastHTML = forecastHTML + "<hr/>";
+  forecastElement.innerHTML = forecastHTML;
+  forecastHTML =
+    forecastHTML +
+    `<footer><a href="https://github.com/atudor913/Vanilla-Weather-App">Open Source-Code</a> by Aisha-Noor Tudor</footer>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+/*API WEATHER INTERGRATION*/
+
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "10d1c4efa3b825ba6b0336d233f5ea47";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showForecast);
+}
+
+/*DISPLAY WEATHER*/
 function showWeather(response) {
   console.log(response);
   document.querySelector("#city-name").innerHTML = response.data.name;
@@ -18,7 +111,11 @@ function showWeather(response) {
     );
 
   celciusTemp = response.data.main.temp;
+
+  getForecast(response.data.coord);
 }
+
+/*SEARCH DATA INTERGRATION*/
 
 function showCity(city) {
   let apiKey = "10d1c4efa3b825ba6b0336d233f5ea47";
@@ -59,43 +156,6 @@ celcius.addEventListener("click", showCelcius);
 /*(0°C × 9/5) + 32 = 32°F
  */
 
+/*FUNCTION CALLS*/
+
 showCity("Brooklyn");
-
-/*DATE INTERGRATION*/
-let setting = new Date();
-
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-
-let months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-
-let currentDay = days[setting.getDay()];
-let currentMin = setting.getMinutes();
-let currentHour = setting.getHours();
-let currentMonth = months[setting.getMonth()];
-
-let currentSettings = `${currentDay} ${currentHour}:${currentMin}`;
-console.log(currentSettings);
-
-let inputTime = document.querySelector("#time-date");
-inputTime.innerHTML = currentSettings;
